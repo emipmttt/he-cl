7a0e2e25086d090406ee96cc4bcd121030a7c7b0
emipmt <emipmttt@gmail.com>
2018-12-18 20:11:20 -0600

    Actualización

--- php/users/delete.php
+++ php/users/delete.php
@@ -4,15 +4,15 @@
 
   $user = $_POST['user'];
 
-  $sqlFindUsers= "SELECT * FROM users WHERE id = '{$user}'";
+  $sqlFindUsers= "SELECT * FROM users WHERE id = '{$user}' ";
   $resultFindUsers = mysqli_query($conn, $sqlFindUsers);
   if (mysqli_num_rows($resultFindUsers) > 0) {
-      $rowFindUsers= mysqli_fetch_assoc($resultFindUsers);
+      $rowFindUsers = mysqli_fetch_assoc($resultFindUsers);
       $campains = json_decode($rowFindUsers['campains']);
       $tablesToDelete = array();
       foreach ($campains as $campain) {
-        $md5 = md5($user.'secretcodepez'.mb_strtolower($campain->title));
-        array_push($tablesToDelete,'reactives_'+$md5);
+        $md5 = md5($user.'secretcodepez'.mb_strtolower($campain['title']));
+        array_push($tablesToDelete,'reactives_'.$md5);
         mysqli_query($conn, "DELETE FROM questionnaires WHERE user = '{$user}' AND campain = '{$campain}' ");
       }
       unset($campain);
@@ -44,6 +44,5 @@
       $response->status = false;
       $response->message = "No existe este usuario";
       $response->sql = $sqlFindUsers;
-      $response->results = $resultFindUsers;
       echo json_encode($response);
   }
