c10959eaa81661d757dc5cecc19194b0f70599e3
Emiliano Pamont <emipmttt@gmail.com>
2019-03-03 01:01:55 -0600

    Seguridad añadida al módulo de adminstrador

--- dev/admin/src/App.vue
+++ dev/admin/src/App.vue
@@ -1,88 +1,93 @@
 <template>
-<div id="app">
-  <ul id="slide-out" class="sidenav sidenav-fixed white">
-    <li class="center">
-      <img src="./assets/logo.png" alt="human express" class="responsive-img sidenav-logo">
-    </li>
-    <li>
-      <router-link class="sidenav-close" to="/">
-        <i class="material-icons">people</i>
-        Clientes
-      </router-link>
-    </li>
-    <li>
-      <a href="/usuarios">
-        <i class="material-icons">person</i>
-        Area de clientes</a>
-    </li>
-    <li>
-      <div class="divider"></div>
-    </li>
-    <li>
-      <a class="subheader">Gestión</a>
-    </li>
-    <li>
-      <a href="#!">
-        <i class="material-icons">all_inbox</i>
-        Exportar
-      </a>
-    <li>
-      <div class="divider"></div>
-    </li>
-    <li>
-      <a class="subheader">Cuestionario base</a>
-    </li>
-    <li>
-      <router-link class="sidenav-close" to="/reactivos">
-        <i class="material-icons">bookmarks</i>
-        Reactivos
-      </router-link>
-    </li>
-    <li>
-      <router-link class="sidenav-close" to="/aspectos">
-        <i class="material-icons">bookmark</i>
-        Aspectos
-      </router-link>
-    </li>
-    <li>
-      <router-link class="sidenav-close" to="/condensados">
-        <i class="material-icons">group_work</i>
-        Condensados
-      </router-link>
-    </li>
-  </ul>
+  <div id="app">
+    <ul id="slide-out" class="sidenav sidenav-fixed white">
+      <li class="center">
+        <img src="./assets/logo.png" alt="human express" class="responsive-img sidenav-logo">
+      </li>
+      <li>
+        <router-link class="sidenav-close" to="/">
+          <i class="material-icons">people</i>
+          Clientes
+        </router-link>
+      </li>
+      <li>
+        <a href="/usuarios">
+          <i class="material-icons">person</i>
+          Area de clientes</a>
+      </li>
+      <li>
+        <div class="divider"></div>
+      </li>
+      <li>
+        <a class="subheader">Gestión</a>
+      </li>
+      <li>
+        <a href="#!">
+          <i class="material-icons">all_inbox</i>
+          Exportar
+        </a>
+      <li>
+        <div class="divider"></div>
+      </li>
+      <li>
+        <a class="subheader">Cuestionario base</a>
+      </li>
+      <li>
+        <router-link class="sidenav-close" to="/reactivos">
+          <i class="material-icons">bookmarks</i>
+          Reactivos
+        </router-link>
+      </li>
+      <li>
+        <router-link class="sidenav-close" to="/aspectos">
+          <i class="material-icons">bookmark</i>
+          Aspectos
+        </router-link>
+      </li>
+      <li>
+        <router-link class="sidenav-close" to="/condensados">
+          <i class="material-icons">group_work</i>
+          Condensados
+        </router-link>
+      </li>
+    </ul>
 
 
-  <header class="navbar-fixed hide-on-large-only">
-    <nav>
-      <div class="nav-wrapper grey lighten-4">
-        <a class="brand-logo navbar-logo"><img src="./assets/logo.png" alt="human express" class="responsive-img"></a>
-        <a data-target="slide-out" class="sidenav-trigger indigo-text"><i class="material-icons">menu</i></a>
-      </div>
-    </nav>
-  </header>
+    <header class="navbar-fixed hide-on-large-only">
+      <nav>
+        <div class="nav-wrapper grey lighten-4">
+          <a class="brand-logo navbar-logo"><img src="./assets/logo.png" alt="human express" class="responsive-img"></a>
+          <a data-target="slide-out" class="sidenav-trigger indigo-text"><i class="material-icons">menu</i></a>
+        </div>
+      </nav>
+    </header>
 
-  <main>
-    <router-view />
-  </main>
-</div>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      screen: screen
+  export default {
+    name: 'App',
+    data() {
+      return {
+        screen: screen
+      }
+    },
+    mounted() {
+      if (JSON.parse(localStorage.sessionData).type != 1) {
+        location.href = "/"
+      }
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, {});
     }
-  },
-  mounted() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
   }
-}
+
 </script>
 
 <style>
-@import './assets/css/global.css';
+  @import './assets/css/global.css';
+
 </style>
