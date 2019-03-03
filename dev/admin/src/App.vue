079be4ef9a0bc0f4f9bea3a5b8e110c0cf2db066
Emiliano Pamont <emipmttt@gmail.com>
2019-03-03 13:19:13 -0600

    Cambios

--- dev/admin/src/App.vue
+++ dev/admin/src/App.vue
@@ -1,78 +1,81 @@
 <template>
   <div id="app">
-    <ul id="slide-out" class="sidenav sidenav-fixed white">
-      <li class="center">
-        <img src="./assets/logo.png" alt="human express" class="responsive-img sidenav-logo">
-      </li>
-      <li>
-        <router-link class="sidenav-close" to="/">
-          <i class="material-icons">people</i>
-          Clientes
-        </router-link>
-      </li>
-      <li>
-        <a href="/usuarios">
-          <i class="material-icons">person</i>
-          Area de clientes</a>
-      </li>
-      <li>
-        <div class="divider"></div>
-      </li>
-      <li>
-        <a class="subheader">Gestión</a>
-      </li>
-      <li>
-        <a href="#!">
-          <i class="material-icons">all_inbox</i>
-          Exportar
-        </a>
-      <li>
-        <div class="divider"></div>
-      </li>
-      <li>
-        <a class="subheader">Cuestionario base</a>
-      </li>
-      <li>
-        <router-link class="sidenav-close" to="/reactivos">
-          <i class="material-icons">bookmarks</i>
-          Reactivos
-        </router-link>
-      </li>
-      <li>
-        <router-link class="sidenav-close" to="/aspectos">
-          <i class="material-icons">bookmark</i>
-          Aspectos
-        </router-link>
-      </li>
-      <li>
-        <router-link class="sidenav-close" to="/condensados">
-          <i class="material-icons">group_work</i>
-          Condensados
-        </router-link>
-      </li>
-    </ul>
-    <li>
-      <div class="divider"></div>
-    </li>
-    <li>
-      <a @click="logout">
-        <i class="material-icons">exit_to_app</i>
-        Salir
-      </a>
-    </li>
+    <div v-if="isAdmin">
+      <ul id="slide-out" class="sidenav sidenav-fixed white">
+        <li class="center">
+          <img src="./assets/logo.png" alt="human express" class="responsive-img sidenav-logo">
+        </li>
+        <li>
+          <router-link class="sidenav-close" to="/">
+            <i class="material-icons">people</i>
+            Clientes
+          </router-link>
+        </li>
+        <li>
+          <a href="/usuarios">
+            <i class="material-icons">person</i>
+            Area de clientes</a>
+        </li>
+        <li>
+          <div class="divider"></div>
+        </li>
+        <li>
+          <a class="subheader">Gestión</a>
+        </li>
+        <li>
+          <a href="#!">
+            <i class="material-icons">all_inbox</i>
+            Exportar
+          </a>
+        <li>
+          <div class="divider"></div>
+        </li>
+        <li>
+          <a class="subheader">Cuestionario base</a>
+        </li>
+        <li>
+          <router-link class="sidenav-close" to="/reactivos">
+            <i class="material-icons">bookmarks</i>
+            Reactivos
+          </router-link>
+        </li>
+        <li>
+          <router-link class="sidenav-close" to="/aspectos">
+            <i class="material-icons">bookmark</i>
+            Aspectos
+          </router-link>
+        </li>
+        <li>
+          <router-link class="sidenav-close" to="/condensados">
+            <i class="material-icons">group_work</i>
+            Condensados
+          </router-link>
+        </li>
+        <li>
+          <div class="divider"></div>
+        </li>
+        <li>
+          <a @click="logout">
+            <i class="material-icons">exit_to_app</i>
+            Salir
+          </a>
+        </li>
+      </ul>
 
-    <header class="navbar-fixed hide-on-large-only">
-      <nav>
-        <div class="nav-wrapper grey lighten-4">
-          <a class="brand-logo navbar-logo"><img src="./assets/logo.png" alt="human express" class="responsive-img"></a>
-          <a data-target="slide-out" class="sidenav-trigger indigo-text"><i class="material-icons">menu</i></a>
-        </div>
-      </nav>
-    </header>
 
-    <main>
-      <router-view />
-    </main>
+      <header class="navbar-fixed hide-on-large-only">
+        <nav>
+          <div class="nav-wrapper grey lighten-4">
+            <a class="brand-logo navbar-logo"><img src="./assets/logo.png" alt="human express" class="responsive-img"></a>
+            <a data-target="slide-out" class="sidenav-trigger indigo-text"><i class="material-icons">menu</i></a>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -96,6 +99,19 @@
           }
         } else {
           location.href = "/"
+        }
+      }
+    },
+    computed: {
+      isAdmin() {
+        if (localStorage.sessionData) {
+          if (JSON.parse(localStorage.sessionData).type) {
+            return true
+          } else {
+            return false
+          }
+        } else {
+          return false
         }
       }
     },
