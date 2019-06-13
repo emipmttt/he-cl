2967fc16f21cf6aaab211d138a7cb54edf2c8a87
Emiliano Pamont <emipmttt@gmail.com>
2019-06-13 10:32:00 -0500

    Grafica global parcial

--- results/js/components/globalParcial.js
+++ results/js/components/globalParcial.js
@@ -22,12 +22,10 @@ Vue.component("global-parcial-chart", {
     buildDataToUse() {
       var params = [];
 
-      console.log(this.data)
       this.data.forEach(element => {
         params.push(element[this.type]);
       });
 
-      console.log(params)
 
       var unique = (value, index, self) => {
         return self.indexOf(value) === index;
@@ -49,9 +47,11 @@ Vue.component("global-parcial-chart", {
           questionnaires
         })
 
-      });
 
-      console.log(this.type);
+      });
+      // console.log(paramsBuilt);
+
+      // console.log(this.type);
 
       // console.log(params);
       // console.log(paramsBuilt);
@@ -61,10 +61,45 @@ Vue.component("global-parcial-chart", {
       this.buildChart(paramsBuilt);
 
     },
+    answered(aspect, value) {
+
+      if (isNaN(this.aspectsList[aspect])) {
+        this.aspectsList[aspect] = value;
+      } else {
+        this.aspectsList[aspect] = this.aspectsList[aspect] + value;
+      }
+
+    },
+    addAspectsTotalList() {
+      JSON.parse(this.questionnaires[0].reactivesAnswers).forEach(reactive => {
+        let aspect = reactive.aspect;
+
+        // console.log(aspect);
+
+        if (isNaN(this.aspectsTotalList[aspect])) {
+          this.aspectsTotalList[aspect] = 1;
+        } else {
+          this.aspectsTotalList[aspect]++;
+        }
+      });
+      // console.log(this.aspectsTotalList);
+    },
+    calculateAspects() {
+      let aspectsCalculated = {};
+      Object.keys(this.aspectsTotalList).forEach(key => {
+        aspectsCalculated[key] =
+          this.aspectsList[key] / this.aspectsTotalList[key];
+      });
+      this.calculatedAspects = aspectsCalculated;
+    },
+    newBuiltChart() {
+
+
+    },
+
 
     buildChart(params) {
-
-      console.log(params);
+      // console.log(params);
       var labels = [];
       var datasets = [{
         label: "Porcentaje %",
@@ -73,64 +108,95 @@ Vue.component("global-parcial-chart", {
       }];
 
       this.valuesBuilt = params;
-      var totalValues = [];
-      var labels = [];
+      var areas = [];
+      var valores = [];
+      var backgroundColor = [];
 
-      params.forEach(param => { 
+      params.forEach(param => {
 
-        console.log(param);
+        let reactives = JSON.parse(param.questionnaires[0].reactivesAnswers);
+        let aspectsLength = {}
+        let aspectsValues = {}
+        let reactivesValues = {};
+        let reactivesLength = {};
+        // obtener valores;
+        for (let index = 0; index < reactives.length; index++) {
 
-        let aspects = {};
-        let aspectsParticipants = 0;
 
-        param.questionnaires.forEach(questionnaire => {
-          let aspect = JSON.parse(questionnaire.aspects);
-          Object.keys(aspect).sort().forEach(key => {
+          let reactiveMedia = 0;
+          let aspect = "";
 
-            if (isNaN(aspects[key])) {
-              aspects[key] = aspect[key];
-            } else {
-              aspects[key] += aspect[key];
-            }
+          param.questionnaires.forEach(questionnaire => {
 
-          });
+            let reactive = JSON.parse(questionnaire.reactivesAnswers)[index];
 
-          aspectsParticipants++;
-        });
+            reactiveMedia += ((5 * (reactive.value * 10)) / 5);
+            aspect = reactive.aspect;
 
+
+          })
+          if (isNaN(reactivesValues[aspect])) {
+            reactivesValues[aspect] = (reactiveMedia / param.questionnaires.length) / 10;
+          } else {
+            reactivesValues[aspect] += (reactiveMedia / param.questionnaires.length) / 10;
+          }
+          if (isNaN(reactivesLength[aspect])) {
+            reactivesLength[aspect] = 1;
+          } else {
+            reactivesLength[aspect]++;
+          }
+
+        }
+
+        let aspectsMedia = {}
+        Object.keys(reactivesValues).forEach(reactive => {
+          aspectsMedia[reactive] = (((reactivesValues[reactive] / reactivesLength[reactive]) * 100) / 5).toFixed(2)
+        })
+
+        // console.log(aspectsMedia);
+
+
+
+        var labels = [];
         var data = [];
-        var backgroundColor = [];
         var total = 0;
 
         var globalData = [];
-        labels.push(param.title);
 
-        Object.keys(aspects).forEach(key => {
-          total += (aspects[key] / aspectsParticipants);
-          aspects[key] = (((aspects[key] / aspectsParticipants) / 5) * 100).toFixed(1);
-          // labels.push(key);
-          // backgroundColor.push("#bcd6ff");
-          // data.push(aspects[key]);
+
+        Object.keys(aspectsMedia).forEach(key => {
+          // console.log(aspects)
+          total += parseFloat(aspectsMedia[key]);
+          // aspectsMedia[key] = (((aspectsMedia[key] / aspectsParticipants) / 5) * 100).toFixed(1);
+          labels.push(key);
+          data.push(aspectsMedia[key]);
 
           globalData.push({
             aspect: key,
-            value: aspects[key]
+            value: aspectsMedia[key]
           })
 
         });
 
-        this.dataText = {
-          labels,
-          totalValues
-        };
 
-        // labels.push("Total");
-        // backgroundColor.push("#3f51b5");
-        // data.push((((total / Object.keys(aspects).length) / 5) * 100).toFixed(1));
-        totalValues.push((((total / Object.keys(aspects).length) / 5) * 100).toFixed(1))
+        total = (total / Object.keys(aspectsMedia).length).toFixed(2);
+
+        // this.abcGlobalData = globalData;
+
+
+        this.globalData = globalData;
+        // backgroundColor.push("#bcd6ff");
+
+        backgroundColor.push("#3f51b5");
+        valores.push(parseFloat(total));
+        areas.push(param.title);
 
 
       });
+
+      console.log(areas, valores)
+      data = valores;
+      labels = areas;
       setTimeout(() => {
           let ctx = document.getElementById('entitie-global-chart-' + this.type).getContext('2d');
           let aspectChartGlobal = new Chart(ctx, {
@@ -139,8 +205,8 @@ Vue.component("global-parcial-chart", {
               labels,
               datasets: [{
                 label: "Porcentaje %",
-                data: totalValues,
-                backgroundColor: "#3f51b5",
+                data,
+                backgroundColor,
               }]
             },
             options: {
@@ -164,69 +230,11 @@ Vue.component("global-parcial-chart", {
             }
           });
         },
-        100);
+        1000);
+
+
     }
 
-    // buildChart(params) {
-    //   console.log(params);
-    //   var labels = [];
-    //   var datasets = [{
-    //     label: "Porcentaje %",
-    //     data: [],
-    //     backgroundColor: "#3f51b5"
-    //   }];
-
-    //   params.forEach(param => {
-
-    //     labels.push(param.title);
-
-    //     var porcentagePre = 0;
-    //     var porcentageMaxPre = param.questionnaires.length;
-
-    //     param.questionnaires.forEach(questionnaire => {
-    //       let aspectsResults = JSON.parse(questionnaire.aspects)
-    //       Object.keys(aspectsResults).forEach(key => {
-    //         porcentagePre += aspectsResults[key];
-    //       })
-    //     })
-    //     console.log(porcentagePre, porcentageMaxPre)
-    //     datasets[0].data.push(((porcentagePre / porcentageMaxPre)).toFixed(1));
-
-    //   });
-
-
-
-    //   setTimeout(() => {
-    //       let ctx = document.getElementById('entitie-global-chart-' + this.type).getContext('2d');
-    //       let aspectChartGlobal = new Chart(ctx, {
-    //         type: 'bar',
-    //         data: {
-    //           labels,
-    //           datasets,
-    //         },
-    //         options: {
-    //           responsive: true,
-    //           scales: {
-    //             xAxes: [{
-    //               ticks: {
-    //                 fontSize: 16,
-    //                 autoSkip: true,
-    //               }
-    //             }],
-
-    //             yAxes: [{
-    //               ticks: {
-    //                 beginAtZero: true,
-    //                 max: 100,
-    //                 fontSize: 20,
-    //               }
-    //             }]
-    //           }
-    //         }
-    //       });
-    //     },
-    //     100);
-    // }
 
   },
   mounted() {
