eee072b8dd04d26cbdc20a5c83120ac4d7374e39
emipmt <emipmt@gmail.com>
2019-10-13 18:09:07 -0500

    Solución de actulizar rangos

--- dev/admin/src/components/aspects/textualRanges.vue
+++ dev/admin/src/components/aspects/textualRanges.vue
@@ -1,25 +1,36 @@
 <template>
-
   <div>
     <form @submit.prevent="send">
       <div class="input-field">
         <i class="material-icons prefix">assignment_ind</i>
-        <input class="validate" v-bind:id="'diagnosis' + aspectId" v-model:value="diagnosis" type="text" maxlength="700"
-          required>
+        <input
+          class="validate"
+          v-bind:id="'diagnosis' + aspectId"
+          v-model="diagnosis"
+          type="text"
+          maxlength="700"
+          required
+        />
         <label v-bind:for="'diagnosis' + aspectId">Diagnóstico</label>
       </div>
       <div class="input-field">
         <i class="material-icons prefix">assignment</i>
-        <input class="validate" v-bind:id="'recommendations' + aspectId" v-model:value="recommendations" type="text"
-          maxlength="700" required>
+        <input
+          class="validate"
+          v-bind:id="'recommendations' + aspectId"
+          v-model="recommendations"
+          type="text"
+          maxlength="700"
+          required
+        />
         <label v-bind:for="'recommendations' + aspectId">Recomendaciones</label>
       </div>
 
       <div class="row">
         <div class="col s12 m6">
           <label>Rango</label>
-          <select class="browser-default" v-model:value="rangeNum" required>
-            <option value="" disabled selected>Selecciona un Rango</option>
+          <select class="browser-default" v-model="rangeNum" required>
+            <option value disabled selected>Selecciona un Rango</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -36,9 +47,8 @@
             </button>
           </div>
         </div>
-        <p class="indigo-text"> {{ response }} </p>
+        <p class="indigo-text">{{ response }}</p>
       </div>
-
     </form>
     <div class="divider"></div>
 
@@ -57,102 +67,106 @@
             <i class="material-icons left">assignment</i>
             Recomendaciones
           </th>
-          <th>
-            Acciones
-          </th>
+          <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
-        <tr is="textualRangeItem" @update:change="get" :key="textualRange.id" v-for="textualRange in textualRanges"
-          :textualRange="textualRange">
-        </tr>
+        <tr
+          is="textualRangeItem"
+          @update:change="get"
+          :key="textualRange.id"
+          v-for="textualRange in textualRanges"
+          :textualRange="textualRange"
+        ></tr>
       </tbody>
     </table>
   </div>
-
 </template>
 <script>
-  import axios from 'axios';
-  import loading from '@/components/loading';
-  import textualRangeItem from '@/components/aspects/textualRangeItem';
-  export default {
-    name: 'textual-rangeNums',
-    props: {
-      aspect: Object
+import axios from "axios";
+import loading from "@/components/loading";
+import textualRangeItem from "@/components/aspects/textualRangeItem";
+export default {
+  name: "textual-rangeNums",
+  props: {
+    aspect: Object
+  },
+  data() {
+    return {
+      title: this.aspect.title,
+      aspectId: this.aspect.id,
+      diagnosis: "",
+      recommendations: "",
+      rangeNum: "",
+      textualRanges: [],
+
+      //create form
+      response: "",
+      buttonDisabled: false,
+      deleted: false
+    };
+  },
+  methods: {
+    send() {
+      let data = {
+        dependency: this.aspectId,
+        diagnosis: this.diagnosis,
+        recommendations: this.recommendations,
+        rangeNum: this.rangeNum,
+        typeDependency: "aspect"
+      };
+      console.log(data);
+      this.buttonDisabled = true;
+      axios
+        .post(
+          "https://clima-laboral.human-express.com/php/textualRanges/create.php",
+          this.createFormData(data)
+        )
+        .then(response => {
+          this.buttonDisabled = false;
+          console.log(response);
+          if (response.data.status) {
+            M.toast({
+              html: response.data.message
+            });
+            this.get();
+            this.response = "";
+          } else {
+            this.response = response.data.message;
+          }
+        })
+        .catch(error => {
+          this.buttonDisabled = false;
+          this.response = "No se pudo enviar la información" + error;
+        });
     },
-    data() {
-      return {
-        title: this.aspect.title,
-        aspectId: this.aspect.id,
-        diagnosis: '',
-        recommendations: '',
-        rangeNum: '',
-        textualRanges: [],
-
-        //create form
-        response: '',
-        buttonDisabled: false,
-        deleted: false
-
-      }
-    },
-    methods: {
-      send() {
-        let data = {
-          dependency: this.aspectId,
-          diagnosis: this.diagnosis,
-          recommendations: this.recommendations,
-          rangeNum: this.rangeNum,
-          typeDependency: 'aspect'
-        }
-        console.log(data);
-        this.buttonDisabled = true;
-        axios
-          .post('https://clima-laboral.human-express.com/php/textualRanges/create.php', this.createFormData(data))
-          .then(response => {
-            this.buttonDisabled = false;
-            console.log(response);
-            if (response.data.status) {
-              M.toast({
-                html: response.data.message
-              });
-              this.get();
-              this.response = '';
-            } else {
-              this.response = response.data.message;
-            }
-          })
-          .catch(error => {
-            this.buttonDisabled = false;
-            this.response = "No se pudo enviar la información" + error;
-
-          })
-      },
-      get() {
-        axios.get('https://clima-laboral.human-express.com/php/textualRanges/read.php?query=dependency&dependency=' +
+    get() {
+      axios
+        .get(
+          "https://clima-laboral.human-express.com/php/textualRanges/read.php?query=dependency&dependency=" +
             this.aspectId +
-            '&typeDependency=aspect')
-          .then(response => {
-            this.textualRanges = response.data.textualRanges;
-            console.log(response);
-          })
-      },
-      createFormData(postData) {
-        var formDa = new FormData();
-        for (var key in postData) {
-          formDa.append(key, postData[key]);
-        }
-        return formDa;
+            "&typeDependency=aspect"
+        )
+        .then(response => {
+          this.textualRanges = response.data.textualRanges;
+          console.log(response);
+        });
+    },
+    createFormData(postData) {
+      var formDa = new FormData();
+      for (var key in postData) {
+        formDa.append(key, postData[key]);
       }
-    },
-    components: {
-      loading,
-      textualRangeItem
-    },
-    mounted() {
-      this.get();
+      return formDa;
     }
+  },
+  components: {
+    loading,
+    textualRangeItem
+  },
+  mounted() {
+    this.get();
   }
-
+};
 </script>
 <style scoped></style>
