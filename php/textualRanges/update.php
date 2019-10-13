961bc01d37cc4aa418b59b1839fc826d3db81db8
emipmt <emipmt@gmail.com>
2019-10-13 17:13:01 -0500

    Editar textos

--- php/textualRanges/update.php
+++ php/textualRanges/update.php
@@ -10,23 +10,15 @@
   $typeDependency = $_POST['typeDependency'];
   $dependency = $_POST['dependency'];
 
-
-  $sqlFindDoubleTextualRange = "SELECT * FROM textualRanges WHERE typeDependency = '{$typeDependency}' AND dependency = '{$dependency}' AND rangeNum = '{$rangeNum}' ";
-  $resultFindDoubleTextualRange = mysqli_query($conn, $sqlFindDoubleTextualRange);
-  if (mysqli_num_rows($resultFindDoubleTextualRange) > 0) {
-    $response->status = false;
-    $response->message = "Campos repetidos, revisa la información";
+  $sqlUpdateTextualRanges = "UPDATE textualRanges SET diagnosis = '{$diagnosis}', recommendations = '{$recommendations}', rangeNum='{$rangeNum}' WHERE id = '{$id}' ";
+  if (mysqli_query($conn, $sqlUpdateTextualRanges)) {
+    $response->status = true;
+    $response->message = 'Rango actualizado correctamente';
+    $response->textualRanges = readTextualRanges($sqlReadTextualRanges,$conn);
     echo json_encode($response);
   } else {
-    $sqlUpdateTextualRanges = "UPDATE textualRanges SET diagnosis = '{$diagnosis}', recommendations = '{$recommendations}', rangeNum='{$rangeNum}' WHERE id = '{$id}' ";
-    if (mysqli_query($conn, $sqlUpdateTextualRanges)) {
-      $response->status = true;
-      $response->message = 'Rango actualizado correctamente';
-      $response->textualRanges = readTextualRanges($sqlReadTextualRanges,$conn);
-      echo json_encode($response);
-    } else {
-      $response->status = false;
-      $response->message = 'Algo salió mal, intentalo nuevamente más tarde';
-      echo json_encode($response);
-    }
+    $response->status = false;
+    $response->message = 'Algo salió mal, intentalo nuevamente más tarde';
+    echo json_encode($response);
   }
+  
